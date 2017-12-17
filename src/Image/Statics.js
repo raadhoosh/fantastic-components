2f1b99e83555257fb7563a58dd091e203a1095a3
alesaadi13 <al952016@gmail.com>
2017-12-17 16:28:33 +0330

    change Image

---
+++ src/Image/Statics.js
@@ -0,0 +1,47 @@
+import defaultTheme from '../defaultTheme';
+
+function getBorderC(props) {
+  const {
+    primary,
+    secondary,
+    info,
+    warning,
+    danger,
+    success,
+    inverse,
+    borderColor,
+    theme
+  } = props;
+
+  let BorderC;
+  if (primary) {
+    BorderC = theme.primary.borderColor;
+  }
+  else if (secondary) {
+    BorderC = theme.secondary.borderColor;
+  }
+  else if (info) {
+    BorderC = theme.info.borderColor;
+  }
+  else if (warning) {
+    BorderC = theme.warning.borderColor;
+  }
+  else if (danger) {
+    BorderC = theme.danger.borderColor;
+  }
+  else if (success) {
+    BorderC = theme.success.borderColor;
+  }
+  else if (inverse) {
+    BorderC = theme.inverse.borderColor;
+  }
+
+  if (borderColor) {
+    BorderC = borderColor;
+  } else {
+    BorderC = defaultTheme.primary.borderColor;
+  }
+  return BorderC;
+};
+
+export {getBorderC};
