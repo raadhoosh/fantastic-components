11a836b50d32d7aa1fb35f69eb88d1b7df27453c
nassim <nassim.mehri@gmail.com>
2018-01-01 16:08:17 +0330

    create List component.

---
+++ src/List/List/style/HolderStyled.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Programmer1 on 12/4/2017.
+ */
+import styled from 'styled-components';
+import { getLightBgColor, getBorderColor } from './Statics';
+
+const HolderStyled = styled.div`
+      font-family: ${(props) => props.theme.fontFamily};
+      background-color:${(props) => getLightBgColor(props)};  
+      direction: ${(props) => (props.rtl || props.theme.rtl) ? 'rtl' : 'ltr' };
+      border: 1px solid  ${(props) => getBorderColor(props)};
+      border-radius: 5px;   
+      position: relative;
+      min-height: 1em;
+      line-height: 1.4285em;
+      font-size: 1em;
+    `;
+
+export default HolderStyled;
---
+++ src/List/List/style/ItemStyled.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Programmer1 on 12/4/2017.
+ */
+import styled from 'styled-components';
+import {getItemColor} from './Statics';
+
+const LiStyled = styled.li`
+      font-family: ${(props) => props.theme.fontFamily};
+      color:${(props) => getItemColor(props)};  
+      direction: ${(props) => (props.rtl || props.theme.rtl) ? 'rtl' : 'ltr' };
+       ${(props) => props.inLine ? 'display: inline-block;' : 'block'};
+    `;
+
+export default LiStyled;
