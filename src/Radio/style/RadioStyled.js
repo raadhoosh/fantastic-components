1a2d7896648ce1faf19dc55237f415eb081695b0
ahmadbehzadi <ahmad.behzadi@gmail.com>
2018-01-08 15:17:25 +0330

    Fix Radio Error

--- src/Radio/style/RadioStyled.js
+++ src/Radio/style/RadioStyled.js
@@ -1,10 +1,7 @@
-/**
- * Created by Programmer1 on 12/4/2017.
- */
 import styled from 'styled-components';
-import {getColor} from './Statics';
+import { getColor } from './Statics';
 
-const RadioStyled = styled.input` 
+const RadioStyled = styled.input`
           margin: 3px 3px 5px 3px;
           width: 17px;
           height: 17px;
@@ -13,11 +10,8 @@ const RadioStyled = styled.input`
           margin-left:7px;
           cursor: pointer;
           padding: 5px;
-          color:  ${(props) => getColor(props)};                     
-          font-family: ${(props) => props.theme.fontFamily};       
+          color:  ${(props) => getColor(props)};
+          font-family: ${(props) => props.theme.fontFamily};
           direction: ${(props) => (props.rtl || props.theme.rtl) ? 'rtl' : 'ltr' };
-
               `;
-
-
 export default RadioStyled;
